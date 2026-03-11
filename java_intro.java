d09240fc9a956ccbe710a8cc50fe7632979626c3
Mohammad Irfanul Islam <irfanul009@gmail.com>
2026-03-12 00:57:59 +0600

    java_intro again

---
+++ java_intro.java
@@ -0,0 +1,9 @@
+package c251117.c251117_java_basic;
+
+public class java_intro {
+
+    public static void main(String[] args) {
+        String nam = "MOHAMMAD IRFANUL ISLAM";
+        System.out.println("Output = I am " + nam);
+    }
+}
