cfb748a2fe3b4bf7ba65b09b15b50b5b4913d870
Mohammad Irfanul Islam <irfanul009@gmail.com>
2026-03-15 22:18:12 +0600

    java_Get_Started

---
+++ Java_Get_Started.java
@@ -0,0 +1,8 @@
+package c251117.c251117_java_basic;
+
+public class Java_Get_Started {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, let's get start JAVA");
+    }
+}
